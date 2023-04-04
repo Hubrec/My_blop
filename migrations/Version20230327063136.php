005db2443e7aaea85dd2ff7e743283ed23ad876a
GUERIN MATHIS p2105770 <mathis.guerin@etu.univ-lyon1.fr>
2023-04-04 09:28:51 +0200

    fix migrations

--- migrations/Version20230327063136.php
+++ migrations/Version20230327063136.php
@@ -21,7 +21,7 @@ final class Version20230327063136 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE vote (id INT AUTO_INCREMENT NOT NULL, post_id INT NOT NULL, user_id INT NOT NULL, state TINYINT(1) NOT NULL, INDEX IDX_5A1085644B89032C (post_id), INDEX IDX_5A108564A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE vote ADD CONSTRAINT FK_5A1085644B89032C FOREIGN KEY (post_id) REFERENCES post (id)');
+        // $this->addSql('ALTER TABLE vote ADD CONSTRAINT FK_5A1085644B89032C FOREIGN KEY (post_id) REFERENCES post (id)');
         $this->addSql('ALTER TABLE vote ADD CONSTRAINT FK_5A108564A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE post ADD score INT DEFAULT NULL');
     }
